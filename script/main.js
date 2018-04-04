7ca906c7c6fb43ee399024b8678c6efffeb22d1c
Thomas <ThommyDude@users.noreply.github.com>
2018-04-04 21:00:22 +0200

    Update main.js
    added `.item(0)` for obvious reasons!

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {myFunction()};
 
-var menu = document.getElementsByClassName("menu");
+var menu = document.getElementsByClassName("menu").item(0);
 var sticky = menu.offsetTop;
 
 function myFunction() {
