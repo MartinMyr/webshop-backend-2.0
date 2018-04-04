de65ff788f4fc7422492c10034d81bbb14e5578b
danielstena <32764564+danielstena@users.noreply.github.com>
2018-04-04 20:56:13 +0200

    update

--- games.php
+++ games.php
@@ -10,7 +10,55 @@
             <img class="gameImg" src="img/super_mario_cart.jpg">
         </div>
         <div class="cardInfo">
-        hej hopp. Här står det inget viktigt.
+        Hej hopp. Här står det inget viktigt. Bla, bla, bla Bla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, bla.
+        </div>
+        <div class="cardPrice">
+        199:-
+        </div>
+        <div class="amount_submit">
+            <form action="games.php" method="post">
+                <input type="number">
+                <input type="submit" value="add to basket">
+            </form>
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardName">
+            Super Mario Cart
+        </div>
+        <div class="cardImage">
+            <img class="gameImg" src="img/super_mario_cart.jpg">
+        </div>
+        <div class="cardInfo">
+        Hej hopp. Här står det inget viktigt. Bla, bla, bla Bla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, bla.
+        </div>
+        <div class="cardPrice">
+        199:-
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardName">
+            Super Mario Cart
+        </div>
+        <div class="cardImage">
+            <img class="gameImg" src="img/super_mario_cart.jpg">
+        </div>
+        <div class="cardInfo">
+        Hej hopp. Här står det inget viktigt. Bla, bla, bla Bla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, bla.
+        </div>
+        <div class="cardPrice">
+        199:-
+        </div>
+    </div>
+    <div class="card">
+        <div class="cardName">
+            Super Mario Cart
+        </div>
+        <div class="cardImage">
+            <img class="gameImg" src="img/super_mario_cart.jpg">
+        </div>
+        <div class="cardInfo">
+        Hej hopp. Här står det inget viktigt. Bla, bla, bla Bla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, bla.
         </div>
         <div class="cardPrice">
         199:-
@@ -38,49 +86,7 @@
             <img class="gameImg" src="img/super_mario_cart.jpg">
         </div>
         <div class="cardInfo">
-        hej hopp. Här står det inget viktigt.
-        </div>
-        <div class="cardPrice">
-        199:-
-        </div>
-    </div>
-    <div class="card">
-        <div class="cardName">
-            Super Mario Cart
-        </div>
-        <div class="cardImage">
-            <img class="gameImg" src="img/super_mario_cart.jpg">
-        </div>
-        <div class="cardInfo">
-        hej hopp. Här står det inget viktigt.
-        </div>
-        <div class="cardPrice">
-        199:-
-        </div>
-    </div>
-    <div class="card">
-        <div class="cardName">
-            Super Mario Cart
-        </div>
-        <div class="cardImage">
-            <img class="gameImg" src="img/super_mario_cart.jpg">
-        </div>
-        <div class="cardInfo">
-        hej hopp. Här står det inget viktigt.
-        </div>
-        <div class="cardPrice">
-        199:-
-        </div>
-    </div>
-    <div class="card">
-        <div class="cardName">
-            Super Mario Cart
-        </div>
-        <div class="cardImage">
-            <img class="gameImg" src="img/super_mario_cart.jpg">
-        </div>
-        <div class="cardInfo">
-        hej hopp. Här står det inget viktigt.
+        Hej hopp. Här står det inget viktigt. Bla, bla, bla Bla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, blaBla, bla, bla.
         </div>
         <div class="cardPrice">
         199:-
