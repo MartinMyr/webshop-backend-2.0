3ed44b2277a9273287dc72e2e49b86a50b7328e9
danielstena <32764564+danielstena@users.noreply.github.com>
2018-04-05 15:42:50 +0200

    skapat classes.css

---
+++ include/classes.php
@@ -0,0 +1,13 @@
+<?php
+
+    class Products {
+        public $id;
+        public $name;
+        public $price;
+
+        function __construct($name){
+            $this->name = $name;
+        }    
+    }     
+
+?>
