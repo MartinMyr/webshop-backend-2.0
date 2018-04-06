eda37304af0ea497d69be0470bfc4a23e2bc6ba7
MartinMyr <32762130+MartinMyr@users.noreply.github.com>
2018-04-06 11:45:04 +0200

    .

--- functions/functions.php
+++ functions/functions.php
@@ -17,6 +17,45 @@ function AllProducts(){
         die("FEL: " . $conn->connect_error);
     }
 
+<<<<<<< HEAD
+=======
+    return $conn;
+
+}
+function selectedProduct(){
+    $conn = connection();
+
+    $sql = "SELECT pic, productName, info, price, unitsInStock FROM Products WHERE category = '".$_GET['category']."' ";
+    $result = $conn->query($sql);
+        
+    if($result->num_rows > 0){
+        
+        while($row = $result->fetch_assoc()){
+            echo "<div class='card'>";
+            echo "<div class='cardName'>" . $row["productName"] . "</div>";
+            echo "<div class='cardImage'><img src='img/" . $row["pic"] . "' class='gameImg'></div>";
+            echo "<div class='cardInfo'>" . $row["info"] . "</div>";
+            echo "<div class='cardPrice'>Price: " . $row["price"] . ":-</div>";
+            echo "<div class='unitsInStock'>In stock: " . $row["unitsInStock"] . "</div>";
+            echo "<div class='amount_submit'>";
+            echo "<form action='products.php' method='post'>";
+            echo "<input type='number' class='amount'>";
+            echo "<input type='submit' value='add to basket'>";
+            echo "</form>";
+            echo "</div>";
+            echo "</div>";
+        }
+        
+        
+    } else {
+        echo "error";
+    }
+}
+
+function allProducts(){ 
+    $conn = connection();
+
+>>>>>>> parent of 9f46e44... Merge branch 'master' of https://github.com/MartinMyr/webshop-backend-2.0
     $sql = "SELECT pic, productName, info, price, unitsInStock FROM Products";
     $result = $conn->query($sql);
         
