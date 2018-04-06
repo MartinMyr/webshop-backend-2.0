000d292f116d575e4778cc6367b1e7f4cbf8cbda
Thomas <ThommyDude@users.noreply.github.com>
2018-04-06 10:30:52 +0200

    Update classes.php
    created DBObject class that holds the connection.

--- include/classes.php
+++ include/classes.php
@@ -1,13 +1,29 @@
 <?php
 
-    class Products {
+    abstract class DBObject
+    {
+        private $conn;
+
+        function __construct()
+        {
+            $this->conn = new mysqli(/*server, $user, $pass, $db*/);
+        }
+
+        function __destruct()
+        {
+            $this->conn->close();
+        }
+    }
+
+    class Product
+    {
         public $id;
         public $name;
         public $price;
 
-        function __construct($name){
+        function __construct($name)
+        {
             $this->name = $name;
-        }    
-    }     
-
+        }
+    }
 ?>
