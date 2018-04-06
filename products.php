d391db5b9744001665993c42d41d44e2186c57cc
danielstena <32764564+danielstena@users.noreply.github.com>
2018-04-06 20:00:16 +0200

    Update

--- products.php
+++ products.php
@@ -12,7 +12,7 @@
 </div>
 <div class="cards">
     <?php
-    print_r($_POST);
+    // print_r($_POST);
         if (empty($_GET["category"])){
             allProducts();
         }
@@ -31,7 +31,7 @@
          
             $itemArray = array($productByCode["productId"]=>array('name'=>$productByCode["productName"], 'code'=>$productByCode["productId"], 'quantity'=>$_POST["unitsInStock"], 'price'=>$productByCode["price"]));
             
-            // print_r($itemArray);
+            print_r($itemArray);
        
             if(!empty($_SESSION["cart_item"])) {
                 if(in_array($productByCode["code"],array_keys($_SESSION["cart_item"]))) {
