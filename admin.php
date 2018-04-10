3d0eaaa05a8932cbccced6ff60837aa050003828
yepsson <32198001+yepsson@users.noreply.github.com>
2018-04-10 14:47:11 +0200

    admin start

--- admin.php
+++ admin.php
@@ -1,1 +1,29 @@
+<?php
+    include 'include/header.php';
+    include 'functions/functions.php';
+?>
+   
+   <div id="productButtons">
+    <form method="GET">
+    <button type="submit" name="meny" value="users" id="users">Users</button>
+    <button type="submit" name="meny" value="subscribers" id="subscribers">subscribers</button>
+    <button type="submit" name="meny" value="products" id="products">products</button>
+    </form>
+    </div>
+<?php
+     if (empty($_GET["meny"])){
+            echo "Hej och välkommen till Adminsidan";
+        }
+        else{
+            selectedMeny();
+        };
 
+
+?>
+
+
+
+
+<?php
+    include 'include/footer.php';
+?>
