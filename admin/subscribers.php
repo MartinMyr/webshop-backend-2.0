9f3e5a081873f0baceaa11eb779827bc0c6c973c
MartinMyr <martin.myrmarker@gmail.com>
2018-04-11 22:32:49 +0200

    table sub changes

--- admin/subscribers.php
+++ admin/subscribers.php
@@ -6,7 +6,7 @@
         $sql = "SELECT namn, email FROM Subscribers";
         $subs = connection()->query($sql);
         if($subs->num_rows > 0){
-            echo "<table id='subTable'><tr><th>Name</th><th>Email</th></tr>";
+            echo "<table border='1' id='subTable'><tr><th>Name</th><th>Email</th></tr>";
             while($row = $subs->fetch_assoc()){
                 echo "<tr><td>".$row['namn']."</td><td>".$row['email']."</td> </tr>";
             }
