a8668c708114138ffd0d35a47d315dacfe800f7a
yepsson <32198001+yepsson@users.noreply.github.com>
2018-04-11 14:44:22 +0200

    headerknas

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
     include 'functions/functions.php';
 ?>
    <script>$(".menu").empty();
-            $(".menu").append("<div class='linkDiv' onclick='viewOrders()'><a href='#'>Users</a></div><div class='linkDiv' onclick='viewProducts();><a href='#'>Products</a></div><div class='linkDiv'><a href='#'>Subs.</a></div><div class='linkDiv'><a href='#'>Orders</a></div><div class='linkDiv'><a href='#'>Logout</a></div>");
+            $(".menu").append("<div class='linkDiv' onclick='viewOrders();'><a href='#'>Users</a></div><div class='linkDiv' onclick='viewProducts();><a href='#'>Products</a></div><div class='linkDiv'><a href='#'>Subs.</a></div><div class='linkDiv'><a href='#'>Orders</a></div><div class='linkDiv'><a href='#'>Logout</a></div>");
     </script>
    
 
