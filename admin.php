29d464fe7fef1b01e4079398e6552bc88ce2a6fa
MartinMyr <32762130+MartinMyr@users.noreply.github.com>
2018-04-11 13:54:42 +0200

    login

--- admin.php
+++ admin.php
@@ -1,5 +1,8 @@
 <?php
     include 'include/header.php';
+    if(!isset($_SESSION["adminCheck"]) == true){
+        hedear("location: index.php");
+    }
     include 'functions/functions.php';
 ?>
    <script>$(".menu").empty();
