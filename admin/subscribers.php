52d7f5745d857b93a4fec8f1edb960680b7f9e9d
MartinMyr <martin.myrmarker@gmail.com>
2018-04-11 22:58:07 +0200

    a

--- admin/subscribers.php
+++ admin/subscribers.php
@@ -5,13 +5,15 @@
         include '../functions/functions.php';
         $sql = "SELECT namn, email FROM Subscribers";
         $subs = connection()->query($sql);
+        echo "<table border='2' id='subTable'><tr><th>Name</th><th>Email</th></tr>";
         if($subs->num_rows > 0){
-            echo "<table border='2' id='subTable'><tr><th>Name</th><th>Email</th></tr>";
+            
             while($row = $subs->fetch_assoc()){
                 echo "<tr><td>".$row['namn']."</td><td>".$row['email']."</td> </tr>";
             }
-            echo "</table>";
+            
         }
+        echo "</table>";
         connection()->close;
     }
 
