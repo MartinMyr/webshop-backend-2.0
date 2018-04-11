9e4e43029dee98886640f38f53cf0bcbf7a244e7
yepsson <32198001+yepsson@users.noreply.github.com>
2018-04-11 22:54:52 +0200

    wtf

--- admin/products.php
+++ admin/products.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     
 echo "Hej och välkommen till Produktsidan";
 
-echo "<h1>Befintliga produkter</h1><br><div id='adminProds'><div>";
+echo "<h1>Befintliga produkter</h1><br><div id='adminProds'><div><br>";
 
 echo '<div id="newProd"><h4>Lägg till produkt</h4>
 <form method="post">
