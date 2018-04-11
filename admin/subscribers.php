bc43c4cf5710538e4afc9edc4cce785840f02120
MartinMyr <martin.myrmarker@gmail.com>
2018-04-11 22:20:01 +0200

    style table

--- admin/subscribers.php
+++ admin/subscribers.php
@@ -6,15 +6,19 @@
         $sql = "SELECT namn, email FROM Subscribers";
         $subs = connection()->query($sql);
         if($subs->num_rows > 0){
-        
+            echo "<table id='subTable'><tr><th>Name</th><th>Email</th></tr>";
             while($row = $subs->fetch_assoc()){
-                echo $row["namn"];
+                echo "<tr><td>".$row['namn']."</td><td>".$row['email']."</td> </tr>";
             }
+            echo "</table>";
         }
-        }
-        else{
-            echo "error";
-        }
+    }
+
+
+   
+    else{
+        echo "error";
+    }
     
 
 ?>
