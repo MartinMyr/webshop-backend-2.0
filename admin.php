dae9d884c1e71e8c86bce0d950889d02be3a5a75
yepsson <32198001+yepsson@users.noreply.github.com>
2018-04-11 14:55:17 +0200

    ajax

--- admin.php
+++ admin.php
@@ -3,11 +3,12 @@
     if(!isset($_SESSION["adminCheck"])){
         header("location: index.php");
     }
+ 
     include 'include/header.php';
     include 'functions/functions.php';
 ?>
    <script>$(".menu").empty();
-            $(".menu").append("<div class='linkDiv' onclick='viewOrders();'><a href='#'>Orders</a></div><div class='linkDiv' onclick='viewProducts();'><a href='#'>Products</a></div><div class='linkDiv'><a href='#'>Subs.</a></div><div class='linkDiv'><a href='#'>Users</a></div><div class='linkDiv'><a href='#'>Logout</a></div>");
+            $(".menu").append("<div class='linkDiv' onclick='viewOrders();'><a href='#'>Orders</a></div><div class='linkDiv' onclick='viewProducts();'><a href='#'>Products</a></div><div class='linkDiv' onclick='viewSubscribers();'><a href='#'>Subs.</a></div><div class='linkDiv' onclick='viewUsers();'><a href='#'>Users</a></div><div class='linkDiv'><a href='#'>Logout</a></div>");
     </script>
    
 
