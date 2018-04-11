a29c74839b097cfda13b4699eb134bdf73e358a2
MartinMyr <martin.myrmarker@gmail.com>
2018-04-11 23:12:22 +0200

    aaa

--- admin/products.php
+++ admin/products.php
@@ -1,26 +1,46 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-
     
-echo "Hej och välkommen till Produktsidan";
-
-echo "<h1>Befintliga produkter</h1><br><div id='adminProds'><div><br>";
-
-echo '<div id="newProd"><h4>Lägg till produkt</h4>
-<form method="post">
-<input type="text" name="picture" placeholder="Picture">
-<input type="text" name="id" placeholder="Id">
-<input type="text" name="name" placeholder="Name">
-<input type="text" name="info" placeholder="Info">
-<input type="text" name="price" placeholder="Price">
-<button type="submit" >Add product</button>
-</form></div>';
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    include '../functions/functions.php';
+    
+    echo "Hej och välkommen till Produktsidan";
 
 
-}
-else{
-echo "error";
-}
+    $sql = "SELECT productName, productId  FROM Products";
+    $totProd = connection()->query($sql);
+    echo "<ul>";
+    if($totProd->num_rows > 0){
+        
+        while($row = $totProd->fetch_assoc()){
+            echo "<li>" .$row['productName']. "</li><li>" .$row['productId']";
+        }
+        
+    }
+    echo "</ul>";
+    connection()->close;
+   
 
+
+
+    else{
+    echo "error";
+    }
+
+
+
+
+    echo "<h1>Befintliga produkter</h1><br><div id='adminProds'><div><br>";
+
+    echo '<div id="newProd"><h4>Lägg till produkt</h4>
+    <form method="post">
+    <input type="text" name="picture" placeholder="Picture">
+    <input type="text" name="id" placeholder="Id">
+    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="info" placeholder="Info">
+    <input type="text" name="price" placeholder="Price">
+    <button type="submit" >Add product</button>
+    </form></div>';
+
+
+ 
 ?>
