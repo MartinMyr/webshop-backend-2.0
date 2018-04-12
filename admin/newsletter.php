37762c5891bd3dc94ac0114dad8cd2555e497aa9
Jesper Hansson <32198001+yepsson@users.noreply.github.com>
2018-04-12 20:58:06 +0200

    css

--- admin/newsletter.php
+++ admin/newsletter.php
@@ -2,13 +2,17 @@
     if ($_SERVER['REQUEST_METHOD'] == 'GET'){
         include '../functions/functions.php';
     ?>  
+        <div id='newsDiv'>
+            <h2>Newsletter</h2>
+            <p>Här kan du skapa ett nytt Newsletter, ange titel och infon.</p>
         <form id="inputNewsletter" method="post">
         <h3>Title</h3>
         <input type="text" name="newsletterTitle" maxlength="16" required>
         <h3>Message</h3>
-        <textarea name="comment" maxlength="128" form="inputNewsletter" required></textarea>
+        <textarea name="comment" maxlength="128" form="inputNewsletter" required></textarea><br>
         <button type="submit">Send</button>
-        </form>        
+        </form> 
+        </div>       
         <?php
     }
 ?>
