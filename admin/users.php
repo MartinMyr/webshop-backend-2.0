a650043ec6bdf1cd70d5720c5f2942c895e724ac
Jesper Hansson <32198001+yepsson@users.noreply.github.com>
2018-04-12 20:12:20 +0200

    css

--- admin/users.php
+++ admin/users.php
@@ -5,7 +5,7 @@
         include '../functions/functions.php';
         $sql = "SELECT username, email, name FROM User";
         $users = connection()->query($sql);
-        echo "<table border='2' id='subTable'><tr><th>Username</th><th>Email</th><th>Name</th></tr>";
+        echo "<div id='userDiv'><table border='2' id='subTable'><tr><th>Username</th><th>Email</th><th>Name</th></tr>";
         if($users->num_rows > 0){
             
             while($row = $users->fetch_assoc()){
@@ -13,7 +13,7 @@
             }
             
         }
-        echo "</table>";
+        echo "</table></div>";
         connection()->close;
     }
 
