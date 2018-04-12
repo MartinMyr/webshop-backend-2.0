8714e463b288e1d81cc2b1cc35522d116caf74e5
Jesper Hansson <32198001+yepsson@users.noreply.github.com>
2018-04-12 21:28:48 +0200

    lala

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
     include 'include/header.php';
     include 'functions/functions.php';
 ?>
-   
-
-
-
-
-
-
+   <div id='startDiv'>
+<h1>Hej och välkommen till året 1992.</h1>
+<p> År 1992 blev tv-spelskonsolen årets julklapp, här på denna retroinspirerade superawzumsida skickar vi er tillbaka till denna udnerbara 16bits tid.
+    Här kan ni beställa allt man kan önska sig ifrån denna svunna tid. SÅ pass på, gör ditt fynd och upplev varför det avr bättre förr.
+</p>
+</div>
 
 
 
