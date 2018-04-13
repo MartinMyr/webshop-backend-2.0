5d680567233ce555a833a69b4ffcae8543f19584
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-04-13 14:27:08 +0200

    update

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     include 'functions/functions.php';
 ?>
    <div id='startDiv'>
+   <?php if($_SESSION['inloggad'] == true){
+       echo "<h1>Välkommen" . $SESSION_['inloggadNamn'] . "!";
+   }?>
 <h1>Hej och välkommen till året 1992.</h1>
 <p> År 1992 blev tv-spelskonsolen årets julklapp, här på denna retroinspirerade superawzumsida skickar vi er tillbaka till denna udnerbara 16bits tid.
     Här kan ni beställa allt man kan önska sig ifrån denna svunna tid. SÅ pass på, gör ditt fynd och upplev varför det avr bättre förr.
