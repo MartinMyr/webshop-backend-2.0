f093fc3ead2dfea42057e05421190203cc0d3704
danielstena <32764564+danielstena@users.noreply.github.com>
2018-04-16 20:49:11 +0200

    Update

--- thanks.php
+++ thanks.php
@@ -4,17 +4,18 @@
 ?>
 <div id='startDiv'>
     <?php 
+    $id = getLatesOrder();
         if($_SESSION["nameOnUser"] == true && $_SESSION["nameOnUser"] !== "Guest"){
             echo " <h1>Tack för din beställning ".$_SESSION["nameOnUser"].".</h1>
-            <p>Din beställning med ordernr: ".$_SESSION["id"]." kommer skickas så fort som möjligt!</p>";
+            <p>Din beställning med ordernr: ".$id["id"]." kommer skickas så fort som möjligt!</p>";
         }
         else{
             echo "<h1>Tack för din beställning.</h1>
                 <p> 
-                    Här får ser du dina inloggningsuppgifter för att se din beställning med ordernummer:".$_SESSION["id"].".
+                    Här är dina inloggningsuppgifter för att se din beställning med ordernummer: <strong>".$id["id"]."</strong>.
                 </p>
                 <p>
-                    Användarnamn: ".$_SESSION["nameOnUser"]."  
+                    Användarnamn: <strong>".$_SESSION["nameOnUser"]."</strong>  
                 </p>        
                 <p>
                     Lösenord:
