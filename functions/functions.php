a4884a25614fc7916e5140c035d91ab47f172f73
MartinMyr <32762130+MartinMyr@users.noreply.github.com>
2018-04-16 20:17:39 +0200

    show order

--- functions/functions.php
+++ functions/functions.php
@@ -12,7 +12,30 @@
 
 
 
-   
+function showOrder(){
+        $_SESSION["id"];
+    
+       $sql = "SELECT `orderId`, `productId`, `price` FROM `Order_details` WHERE orderId = ".$_SESSION["id"]." ";
+       $result = $conn->query($sql);
+            
+            if($result->num_rows > 0)
+            {
+               while($row = $result->fetch_assoc())
+                {
+         
+                    echo "
+                   
+                   ".$row['orderId']." <br> ".$row['productId']." <br>
+                   ".$row['price']." <br>
+                    
+                    ";
+                }
+         }
+           else
+       {
+              echo "error";
+            }
+        }
 
     function shipping(){
         $conn = connection();
